ac73107943343e9d338e73bb70a51521bc72248a
ipadilhax <ruanpadilha345@gmail.com>
2026-05-20 01:28:22 -0300

    criação dos models cliente e divida

---
+++ VendinhaGR/VendinhaGR/Models/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VendinhaGR.Models
+{
+    public class Cliente
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Nome { get; set; }
+        [Required, MaxLength(11)]
+        public string CPF { get; set; }
+        [Required]
+        public DateTime DataNascimento { get; set; }
+        public int Idade
+        {
+            get
+            {
+                var hoje = DateTime.Today;
+                var idade = hoje.Year - DataNascimento.Year;
+                if (DataNascimento.Date > hoje.AddYears(-idade))
+                    idade--;
+
+                return idade;
+            }
+        }
+        public string Email { get; set; }
+    }
+}
+
---
+++ VendinhaGR/VendinhaGR/Models/Divida.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VendinhaGR.Models
+{
+    public class Divida
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public bool Situacao { get; set; }
+        [Required]
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataPagamento { get; set; }
+        [Required]
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; } = null!;
+    }
+}
