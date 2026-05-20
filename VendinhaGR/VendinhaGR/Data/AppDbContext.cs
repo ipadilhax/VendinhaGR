0d76c18d3d3f8c9ca7baebe08615e7b568e7622b
ipadilhax <ruanpadilha345@gmail.com>
2026-05-20 20:31:32 -0300

    alterações models e criação do contexto de banco de dados

---
+++ VendinhaGR/VendinhaGR/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VendinhaGR.Models;
+
+namespace VendinhaGR.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions <AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Divida> Dividas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>()
+                .HasIndex(c => c.CPF)
+                .IsUnique();
+
+            modelBuilder.Entity<Cliente>()
+                .HasMany(c => c.Dividas)
+                .WithOne(d => d.Cliente)
+                .HasForeignKey(d => d.ClienteId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
