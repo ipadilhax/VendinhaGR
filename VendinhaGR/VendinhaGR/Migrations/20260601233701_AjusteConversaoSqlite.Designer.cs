2af010bf73e24334e04dd4fb2c6b10093c2adaca
ipadilhax <ruanpadilha345@gmail.com>
2026-06-01 20:37:20 -0300

    ajuste de migrations

--- VendinhaGR/VendinhaGR/Migrations/20260601233701_AjusteConversaoSqlite.Designer.cs
+++ VendinhaGR/VendinhaGR/Migrations/20260601233701_AjusteConversaoSqlite.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VendinhaGR.Data;
 
@@ -10,9 +11,11 @@ using VendinhaGR.Data;
 namespace VendinhaGR.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260601233701_AjusteConversaoSqlite")]
+    partial class AjusteConversaoSqlite
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.8");
---
+++ VendinhaGR/VendinhaGR/Migrations/20260601233701_AjusteConversaoSqlite.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VendinhaGR.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteConversaoSqlite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Valor",
+                table: "Dividas",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor",
+                table: "Dividas",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+        }
+    }
+}
